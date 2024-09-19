185ccea6dede3b7d984549107990be9cb065f547
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2024-09-19 23:39:35 +0530

    added PrintBitBoard

---
+++ bitboards.py
@@ -0,0 +1,18 @@
+import constants
+import globals
+
+def PrintBitBoard(bb):
+    shiftMe = 1
+    sq = 0
+    sq64 = 0
+    print()
+    for rank in range(constants.RANK.R8.value, constants.RANK.R1.value - 1, -1): #7-0
+        for file in range(constants.FILE.A.value, constants.FILE.H.value+1): #0-7
+            sq = constants.FR2SQ(file, rank) # calculating the square for 120 bit
+            sq64 = globals.Sq120ToSq64[sq] # converting the square for 64 bit
+            if((shiftMe << sq64) & bb):
+                print("X", end=" ")
+            else:
+                print("-", end=" ")
+        print()
+        
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import constants
 import init
+import bitboards
 
 if __name__ == "__main__":
     init.AllInit()
