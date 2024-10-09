944fdbf9ef658c609a428a8d38d85b5e4b0a9917
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2024-10-09 14:36:56 +0530

    Added: Search Functions: for iterative deepening

--- search.py
+++ search.py
@@ -1,5 +1,9 @@
 from debug import assert_condition
-from constants import MAXGAMEMOVES
+from constants import MAXGAMEMOVES, BRD_SQ_NUM, MAXDEPTH
+from pvtable import ClearPvTable, GetPvLine
+from misc import GetTimeMs
+from math import inf
+from input_output import PrMove
 
 def CheckUp(): # will be called after certain node
     # check if time up or interrupt from GUI
@@ -14,7 +18,20 @@ def isRepetition(board):
     return False
 
 def ClearForSearch(board, info): # clear all the stats , heuristics, searchHistory, searchKillers etc..
-    pass
+    for index in range(13):
+        for index2 in range(BRD_SQ_NUM):
+            board.searchHistory[index][index] = 0
+            
+    for index in range(2):
+        for index2 in range(MAXDEPTH):
+            board.searchKillers[index][index2] = 0
+            
+    ClearPvTable(board.PvTable)
+    board.ply = 0
+    
+    info.starttime = GetTimeMs()
+    info.stopped = 0
+    info.nodes = 0
 
 def AlphaBeta(alpha, beta, depth, board, info, DoNull):
     return 0
@@ -26,6 +43,30 @@ def Quiescence(alpha, beta, board, info):
 
 def SearchPosition(board, info): # class BOARD, class SEARCHINFO
     # iterative deepening, search init
+    # for depth = 1 to maxDepth, for each of these depths we then search with AlphaBeta
+    # our depth depends on the time left on the clock, we don't want to search to depth 10 on first move and lose on time
+    # iterative deepening means searching from depth 1 to depth n 
+    # ? why don't we just search directly on depth n 
+    # * because in iterative deepening first of all we will have our principal variation which will help in pruning more in AlphaBeta & Move Ordering. also we are maitaining the heurisitsc (searchHistory & searchKillers) which will help in move ordering
     
-    pass
     
+    bestMove = 0
+    bestScore = -inf
+    pvMoves = 0
+    
+    ClearForSearch(board, info)
+    
+    #iterative deepening
+    for currentDepth in range(1, info.depth+1):
+        bestScore = AlphaBeta(-inf, inf, currentDepth, board, info, True) # alpha beta
+        
+        # out of time?
+    
+        pvMoves = GetPvLine(currentDepth, board)
+        bestMove = board.PvArray[0]
+        
+        print(f"Depth: {currentDepth} Score: {bestScore} Move: {PrMove(bestMove)} Nodes: {info.nodes}")
+        
+        for pvNum in range(0, pvMoves):
+            print(f"{PrMove(board.PvArray[pvNum])}", end="  ")
+        print()
